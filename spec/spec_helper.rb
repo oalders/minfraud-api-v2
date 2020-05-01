6b0efd3232d3c111bf22e9f0ee5418c1fe070350
Olaf Alders <olaf@wundersolutions.com>
2020-05-01 00:06:08 -0400

    Add a mocked test for AUTHORIZATION_INVALID

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,4 +9,8 @@ if major_version == '2' && minor_version == '7'
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
+
 require 'minfraud'
