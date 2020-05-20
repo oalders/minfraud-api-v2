5732cb6864b24673c526072882211509e049c0a6
Olaf Alders <olaf@wundersolutions.com>
2020-05-20 18:18:51 -0400

    Fix long lines in transaction.rb docs

--- lib/minfraud/components/transaction.rb
+++ lib/minfraud/components/transaction.rb
@@ -8,12 +8,12 @@ module Minfraud
 
       # @!attribute ip_address
       # @return [String] The IP address of the customer placing the order. This
-      # should be passed as a string like "44.55.66.77" or "2001:db8::2:1".
+      #   should be passed as a string like "44.55.66.77" or "2001:db8::2:1".
       attr_accessor :ip_address
 
       # @attribute tag
       # @return [String] A string indicating the likelihood that a transaction
-      # may be fraudulent
+      #   may be fraudulent
       enum_accessor :type,
                     %i[
                       chargeback
@@ -24,29 +24,29 @@ module Minfraud
 
       # @attribute maxmind_id
       # @return [String] A unique eight character string identifying a minFraud
-      # Standard or Premium request. These IDs are returned in the maxmindID
-      # field of a response for a successful minFraud request. This field is
-      # not required, but you are encouraged to provide it, if possible.
+      #   Standard or Premium request. These IDs are returned in the maxmindID
+      #   field of a response for a successful minFraud request. This field is
+      #   not required, but you are encouraged to provide it, if possible.
       attr_accessor :maxmind_id
 
       # @attribute minfraud_id
       # @return [String] A UUID that identifies a minFraud Score, minFraud
-      # Insights, or minFraud Factors request. This ID is returned at /id in
-      # the response. This field is not required, but you are encouraged to
-      # provide it if the request was made to one of these services.
+      #   Insights, or minFraud Factors request. This ID is returned at /id in
+      #   the response. This field is not required, but you are encouraged to
+      #   provide it if the request was made to one of these services.
       attr_accessor :minfraud_id
 
       # @attribute notes
       # @return [String] Your notes on the fraud tag associated with the
-      # transaction. We manually review many reported transactions to improve
-      # our scoring for you so any additional details to help us understand
-      # context are helpful.
+      #   transaction. We manually review many reported transactions to improve
+      #   our scoring for you so any additional details to help us understand
+      #   context are helpful.
       attr_accessor :notes
 
       # @attribute transaction_id
       # @return [String] The transaction ID you originally passed to minFraud.
-      # This field is not required, but you are encouraged to provide it or the
-      # transaction's maxmind_id or minfraud_id
+      #   This field is not required, but you are encouraged to provide it or
+      #   the transaction's maxmind_id or minfraud_id
       attr_accessor :transaction_id
 
       # Creates Minfraud::Components::Transaction instance
