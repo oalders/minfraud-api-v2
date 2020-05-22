5a4f6da3da0b86d50af700a0486eab903e0027a9
Olaf Alders <olaf@wundersolutions.com>
2020-05-22 15:09:25 -0400

    Remove stray debugging

--- spec/components/transaction_report_spec.rb
+++ spec/components/transaction_report_spec.rb
@@ -6,7 +6,6 @@ describe Minfraud::Components::TransactionReport do
   describe '#initialize' do
     context 'with an invalid type' do
       it 'raises an exception' do
-        puts 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
         expect do
           described_class.new(tag: 'foo')
         end.to raise_exception(Minfraud::NotEnumValueError)
