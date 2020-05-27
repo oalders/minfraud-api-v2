ced81d03766328d5ea8596b40ed8d34b5137c3a2
Olaf Alders <olaf@wundersolutions.com>
2020-05-27 12:05:17 -0400

    Don't use webmock in tests

--- Gemfile
+++ Gemfile
@@ -10,5 +10,3 @@ if major_version == '2' && minor_version == '7'
 end
 
 gemspec
-
-gem "webmock", "~> 3.8"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,7 +10,4 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 
-require 'webmock/rspec'
-WebMock.disable_net_connect!(allow_localhost: true)
-
 require 'minfraud'
