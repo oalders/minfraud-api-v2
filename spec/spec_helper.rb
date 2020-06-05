57aaacbe40854c68a89ca66d63e9deab8f85c7ad
Olaf Alders <olaf@wundersolutions.com>
2020-06-05 10:36:50 -0400

    Fix spec_helper.rb on 1.9

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,5 @@ if major_version == '2' && minor_version == '7'
   Coveralls.wear!
 end
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
-
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'minfraud'
